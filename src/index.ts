7fb8bb11bb873d2a449c75f577bae3de5a915e04
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-27 07:23:50 -0300

    Try a few times if there is an error

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
 import { Browser, Builder } from "selenium-webdriver";
 import { Options } from "selenium-webdriver/chrome";
+import { appendSearchLog } from "./appendSearchLog";
 import { screenshotMap } from "./screenshotMap";
 import { searchCep } from "./searchCep";
 
 async function start() {
-  const driver = await new Builder()
+  const webDriver = await new Builder()
     .forBrowser(Browser.CHROME)
-    .setChromeOptions(new Options().headless())
+    .setChromeOptions(
+      new Options().windowSize({ width: 1092, height: 614 }).headless()
+    )
     .build();
 
-  try {
-    const address = await searchCep("04547-130", driver);
+  const maxTryAmount = 5;
+  let tryAmountCounter = 0;
 
-    console.log(address);
+  do {
+    try {
+      const address = await searchCep("04547-130", webDriver);
 
-    await screenshotMap(address, driver);
-  } finally {
-    await driver.quit();
-  }
+      await appendSearchLog(address);
+
+      await screenshotMap(address, webDriver);
+
+      break;
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+
+      tryAmountCounter++;
+    }
+  } while (tryAmountCounter < maxTryAmount);
+
+  await webDriver.quit();
 }
 
 start();
