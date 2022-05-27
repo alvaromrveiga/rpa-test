d70fc8429524818745ab37387e2bc81e3686e660
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-27 07:23:03 -0300

    Improve readability

--- src/searchCep.ts
+++ src/searchCep.ts
@@ -1,31 +1,55 @@
 import { By, Key, until, WebDriver } from "selenium-webdriver";
+import { getUrl } from "./utils/getUrl";
 
 export async function searchCep(
   cep: string,
-  driver: WebDriver
+  webDriver: WebDriver
 ): Promise<string> {
+  await getUrl(
+    webDriver,
+    "https://buscacepinter.correios.com.br/app/endereco/index.php"
+  );
+
+  await inputCep(webDriver, cep);
+
+  await waitCepResults(webDriver);
+
+  const address = await getAddress(webDriver);
+
+  return address;
+}
+
+async function inputCep(webDriver: WebDriver, cep: string) {
   try {
-    await driver.get(
-      "https://buscacepinter.correios.com.br/app/endereco/index.php"
-    );
+    await webDriver.findElement(By.id("endereco")).sendKeys(cep, Key.RETURN);
+  } catch (error) {
+    throw new Error("Could not find the element to input the CEP");
+  }
+}
 
-    await driver.findElement(By.id("endereco")).sendKeys(cep, Key.RETURN);
-
-    await driver.wait(
+async function waitCepResults(webDriver: WebDriver) {
+  try {
+    await webDriver.wait(
       until.elementTextIs(
-        driver.findElement(By.id("mensagem-resultado")),
+        webDriver.findElement(By.id("mensagem-resultado")),
         "Resultado da Busca por Endereço ou CEP"
       ),
       5000
     );
+  } catch (error) {
+    throw new Error("Could not find the CEP results");
+  }
+}
 
-    const address = await driver
+async function getAddress(webDriver: WebDriver): Promise<string> {
+  try {
+    const address = await webDriver
       .findElement(By.id("resultado-DNEC"))
       .findElement(By.css("tbody"))
       .getText();
 
     return address;
   } catch (error) {
-    throw new Error("Could not find CEP");
+    throw new Error("Could not find this CEP's address");
   }
 }
