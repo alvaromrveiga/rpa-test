ea54694e98d82c5eeea15874300b9e25a4e1e3ba
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-27 07:40:27 -0300

    Handle possible errors on each function

--- src/screenshotMap.ts
+++ src/screenshotMap.ts
@@ -1,108 +1,137 @@
 import { By, Key, until, WebDriver, WebElement } from "selenium-webdriver";
 import fs from "fs";
+import { getUrl } from "./utils/getUrl";
 
 export async function screenshotMap(address: string, webDriver: WebDriver) {
-  try {
-    await webDriver.get("https://www.google.com.br/");
+  await getUrl(webDriver, "https://www.google.com.br/");
 
-    await searchForAddress(webDriver, address);
+  await searchForAddress(webDriver, address);
 
-    await clickOnMapsTab(webDriver);
+  await clickOnMapsTab(webDriver);
 
-    const canvas = await getMapCanvas(webDriver);
+  const canvas = await getMapCanvas(webDriver);
 
-    await closeSideTab(webDriver);
+  await closeSideTab(webDriver);
 
-    await waitCanvasFullyLoad(webDriver);
+  await waitCanvasFullyLoad(webDriver);
 
-    await clickOnSatelliteViewButton(webDriver, canvas);
+  await clickOnSatelliteViewButton(webDriver, canvas);
 
-    await waitMapFullyLoad(webDriver);
+  await waitMapFullyLoad(webDriver);
 
-    await takeAndSaveScreenshot(webDriver);
-  } catch (error) {
-    console.log(error);
-  }
+  await takeAndSaveScreenshot(webDriver);
 }
 
 async function searchForAddress(driver: WebDriver, address: string) {
-  await driver
-    .findElement(By.xpath("//input[@name='q']"))
-    .sendKeys(address, Key.RETURN);
+  try {
+    await driver
+      .findElement(By.xpath("//input[@name='q']"))
+      .sendKeys(address, Key.RETURN);
+  } catch (error) {
+    throw new Error("Could not find the element to input the address");
+  }
 }
 
 async function clickOnMapsTab(driver: WebDriver) {
-  await driver
-    .wait(
-      until.elementLocated(
-        By.xpath("//div[@class='hdtb-mitem']//a[text()='Maps']")
-      ),
-      5000
-    )
-    .click();
+  try {
+    await driver
+      .wait(
+        until.elementLocated(
+          By.xpath("//div[@class='hdtb-mitem']//a[text()='Maps']")
+        ),
+        5000
+      )
+      .click();
+  } catch (error) {
+    throw new Error("Could not find the Maps tab");
+  }
 }
 
 async function getMapCanvas(driver: WebDriver): Promise<WebElement> {
-  const canvas = await driver.wait(
-    until.elementLocated(
-      By.xpath("//canvas[@class='MyME0d widget-scene-canvas']")
-    ),
-    5000
-  );
+  try {
+    const canvas = await driver.wait(
+      until.elementLocated(
+        By.xpath("//canvas[@class='MyME0d widget-scene-canvas']")
+      ),
+      5000
+    );
 
-  await driver.wait(until.elementIsEnabled(canvas), 5000);
+    await driver.wait(until.elementIsEnabled(canvas), 5000);
 
-  await makeCanvasVisibleWhenHeadless(driver, canvas);
+    await makeCanvasVisibleWhenHeadless(driver, canvas);
 
-  return canvas;
+    return canvas;
+  } catch (error) {
+    throw new Error("Could not load map canvas");
+  }
 }
 
 async function waitCanvasFullyLoad(driver: WebDriver) {
-  const searchButtons = await driver.wait(
-    until.elementLocated(
-      By.xpath("//button[@class='tXNTee LCTIRd L6Bbsd Tc0rEd T7HQDc']")
-    ),
-    5000
-  );
+  try {
+    const searchButtons = await driver.wait(
+      until.elementLocated(
+        By.xpath("//button[@class='tXNTee LCTIRd L6Bbsd Tc0rEd T7HQDc']")
+      ),
+      5000
+    );
 
-  await driver.wait(until.elementIsEnabled(searchButtons), 5000);
+    await driver.wait(until.elementIsEnabled(searchButtons), 5000);
+  } catch (error) {
+    throw new Error("Could not fully load map canvas");
+  }
 }
 
 async function calculateSatelliteViewButtonPosition(
   canvas: WebElement
 ): Promise<{ satelliteButtonX: number; satelliteButtonY: number }> {
-  const canvasWidth: number = +(await canvas.getAttribute("width"));
-  const canvasHeight: number = +(await canvas.getAttribute("height"));
+  try {
+    const canvasWidth: number = +(await canvas.getAttribute("width"));
+    const canvasHeight: number = +(await canvas.getAttribute("height"));
 
-  const satelliteButtonX = Math.ceil(0 - canvasWidth / 2 + canvasWidth * 0.06);
+    const satelliteButtonX = Math.ceil(
+      0 - canvasWidth / 2 + canvasWidth * 0.06
+    );
 
-  const satelliteButtonY = Math.ceil(
-    0 + canvasHeight / 2 - canvasHeight * 0.13
-  );
+    const satelliteButtonY = Math.ceil(
+      0 + canvasHeight / 2 - canvasHeight * 0.13
+    );
 
-  return { satelliteButtonX, satelliteButtonY };
+    return { satelliteButtonX, satelliteButtonY };
+  } catch (error) {
+    throw new Error(
+      "There was an error calculating the satellite view button position"
+    );
+  }
 }
 
 async function clickOnSatelliteViewButton(
   driver: WebDriver,
   canvas: WebElement
 ) {
-  const { satelliteButtonX, satelliteButtonY } =
-    await calculateSatelliteViewButtonPosition(canvas);
+  try {
+    const { satelliteButtonX, satelliteButtonY } =
+      await calculateSatelliteViewButtonPosition(canvas);
 
-  const actions = driver.actions({ async: true });
-  await actions
-    .move({ origin: canvas, x: satelliteButtonX, y: satelliteButtonY })
-    .click()
-    .perform();
+    const actions = driver.actions({ async: true });
+    await actions
+      .move({ origin: canvas, x: satelliteButtonX, y: satelliteButtonY })
+      .click()
+      .perform();
+  } catch (error) {
+    throw new Error("Could not click on the satellite view button");
+  }
 }
 
 async function closeSideTab(driver: WebDriver) {
-  const sideTabButtonDiv = await driver
-    .findElement(By.xpath("//div[@class='gYkzb']"))
-    .click();
+  try {
+    const sideTabButtonDiv = await driver
+      .findElement(By.xpath("//div[@class='gYkzb']"))
+      .click();
 
-  await waitCloseSideTabAnimation();
+    await waitCloseSideTabAnimation();
+  } catch (error) {
+    throw new Error("Could not close the side tab");
+  }
 }
 
 async function waitCloseSideTabAnimation() {
@@ -110,47 +139,59 @@ async function waitCloseSideTabAnimation() {
 }
 
 async function waitMapFullyLoad(driver: WebDriver) {
-  const mapImageCredits = await driver.findElement(
-    By.xpath("//span[@id='LFaNsb']")
-  );
+  try {
+    const mapImageCredits = await driver.findElement(
+      By.xpath("//span[@id='LFaNsb']")
+    );
 
-  const mapImageDefaultText = await mapImageCredits.getText();
-  const endsWithCommaDefaultTextRegex = new RegExp(
-    `.*\\, ${mapImageDefaultText}$`
-  );
+    const mapImageDefaultText = await mapImageCredits.getText();
+    const endsWithCommaDefaultTextRegex = new RegExp(
+      `.*\\, ${mapImageDefaultText}$`
+    );
 
-  await driver.wait(
-    until.elementTextMatches(mapImageCredits, endsWithCommaDefaultTextRegex),
-    10000
-  );
+    await driver.wait(
+      until.elementTextMatches(mapImageCredits, endsWithCommaDefaultTextRegex),
+      10000
+    );
+  } catch (error) {
+    throw new Error("Could not fully load map satellite view");
+  }
 }
 
 async function makeCanvasVisibleWhenHeadless(
   driver: WebDriver,
   canvas: WebElement
 ) {
-  const canvasStyle = await canvas.getAttribute("style");
-  const canvasStyleDisplayInline = canvasStyle.replace("none", "inline");
+  try {
+    const canvasStyle = await canvas.getAttribute("style");
+    const canvasStyleDisplayInline = canvasStyle.replace("none", "inline");
 
-  await driver.executeScript(
-    `arguments[0].setAttribute('style', '${canvasStyleDisplayInline}')`,
-    canvas
-  );
+    await driver.executeScript(
+      `arguments[0].setAttribute('style', '${canvasStyleDisplayInline}')`,
+      canvas
+    );
+  } catch (error) {
+    throw new Error("Could not change canvas display to inline");
+  }
 }
 
 async function takeAndSaveScreenshot(webDriver: WebDriver) {
-  const screenshot = await webDriver.takeScreenshot();
+  try {
+    const screenshot = await webDriver.takeScreenshot();
 
-  const screenshotName = new Date(Date.now())
-    .toLocaleDateString("pt-br", {
-      dateStyle: "short",
-      timeStyle: "medium",
-    })
-    .replace(/[\s\/]/g, "-");
+    const screenshotName = new Date(Date.now())
+      .toLocaleDateString("pt-br", {
+        dateStyle: "short",
+        timeStyle: "medium",
+      })
+      .replace(/[\s\/]/g, "-");
 
-  await fs.writeFile(
-    `screenshots/${screenshotName}.png`,
-    Buffer.from(screenshot, "base64"),
-    () => {}
-  );
+    await fs.writeFile(
+      `screenshots/${screenshotName}.png`,
+      Buffer.from(screenshot, "base64"),
+      () => {}
+    );
+  } catch (error) {
+    throw new Error("Error while taking a screenshot of the map");
+  }
 }
